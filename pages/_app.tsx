fe0785077b79b1634d346099a6ccd10402586c75
Kevin Pruett <pruett.kevin@gmail.com>
2020-07-06 08:37:46 -0400

    Implement form and routing with params

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
+import Router from "next/router";
 import Head from "next/head";
-import Link from "next/link";
+import { useFormikContext, Formik, Form, Field } from "formik";
+
 import styles from "../styles/Home.module.css";
 
+interface FormValues {
+  name: string;
+  email: string;
+  phone: string;
+  valid_cdl: string | undefined;
+  valid_twic: string | undefined;
+  two_plus_yoe: string | undefined;
+}
+
 export default function Home() {
+  const initialValues: FormValues = {
+    name: "",
+    email: "",
+    phone: "",
+    valid_cdl: undefined,
+    valid_twic: undefined,
+    two_plus_yoe: undefined,
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Drive for Mecca Trucking</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header>
@@ -19,40 +37,106 @@ export default function Home() {
             className={styles.logo}
           />
           <h1 className={styles.title}>
-            Drive for{" "}
-            <a href="http://www.meccatrucking.com" className={styles.blueTopA}>
-              Mecca Trucking
-            </a>
+            Apply to drive for <a href="http://www.meccatrucking.com">Mecca</a>
           </h1>
         </div>
       </header>
 
       <main className={styles.main}>
-        <p className={styles.description}>
-          Applications open soon...stay tuned. 🚛
-        </p>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={(values, actions) => {
+            actions.setSubmitting(false);
 
-        {/* <div className={styles.grid}>
-          <input type="text" placeholder="Full name"></input>
-          <input type="email" placeholder="Email address"></input>
-          <input type="phone" placeholder="Phone number"></input>
-        </div> */}
-      </main>
-
-      {/* <Link href="/apply">
-        <a className={styles.card}>Complete Application</a>
-      </Link> */}
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+            Router.push({
+              pathname: "/apply",
+              query: { ...values },
+            });
+          }}
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer> */}
+          <Form>
+            <div className={styles.fieldset}>
+              <label htmlFor="name">Name</label>
+              <Field
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Lincoln Hawk"
+              />
+            </div>
+
+            <div className={styles.fieldset}>
+              <label htmlFor="email">Email</label>
+              <Field
+                name="email"
+                type="email"
+                placeholder="lincoln.hawk@overthetop.com"
+              />
+            </div>
+
+            <div className={styles.fieldset}>
+              <label htmlFor="phone">Phone</label>
+              <Field name="phone" type="tel" />
+            </div>
+
+            <div className={styles.fieldset}>
+              <label htmlFor="valid_cdl">
+                Do you hold a valid CDL Class A License?
+              </label>
+              <Field as="select" name="valid_cdl">
+                <option value="">Please select</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </Field>
+            </div>
+
+            <div className={styles.fieldset}>
+              <label htmlFor="valid_twic">Do you hold a valid TWIC card?</label>
+              <Field as="select" name="valid_twic">
+                <option value="">Please select</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </Field>
+            </div>
+
+            <div className={styles.fieldset}>
+              <label htmlFor="two_plus_yoe">
+                Do you have 2 or more years of experience driving tractor
+                trailers?
+              </label>
+              <Field as="select" name="two_plus_yoe">
+                <option value="">Please select</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </Field>
+            </div>
+
+            <FormSubmit />
+          </Form>
+        </Formik>
+      </main>
     </div>
+  );
+}
+
+function FormSubmit() {
+  const { values, isSubmitting } = useFormikContext<FormValues>();
+
+  const isAnyFieldEmpty =
+    !Boolean(values.name) ||
+    !Boolean(values.email) ||
+    !Boolean(values.phone) ||
+    !Boolean(values.valid_cdl) ||
+    !Boolean(values.valid_twic) ||
+    !Boolean(values.two_plus_yoe);
+
+  return (
+    <button
+      className={styles.submit}
+      type="submit"
+      disabled={isAnyFieldEmpty || isSubmitting}
+    >
+      Submit
+    </button>
   );
 }
